fe597f680553895da1e569fa5c47ea416ba8e3af
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2016-10-27 19:41:54 +0300

    draft version of path1

--- lab2b/path1.sh
+++ lab2b/path1.sh
@@ -1,50 +1,102 @@
 #!/bin/sh
 
-visited=" "
+wait() {
+	read  key
+}
 
-# dfs() 
-# {
-	# if test "${visited#*$1}" != "$visited"
-	# local v
-	# v=$1
+input="from 1 2 3
+1 from
+2 from 4
+3 from 4
+4 5 2 3
+5 4 to
+to 5"
 
-	# if contains $visited $v
-	# then
-	# 	visited+="$1 "
-	# if [ $1 -le 1 ]
- #        then
- #              return 1
+curr_adj_list=""
 
-# }
+find_adj_list() {
+	curr_adj_list=$(echo "$input" | sed -n "/^$1/p")
+	# echo "$input" | while read a; do
+		# echo "$a"
+	# done
+}
+
+# echo $(find_adj_list from)
+# find_adj_list from
+# echo $curr_adj_list
+
+# exit
 
 contains() {
     string="$1"
     substring="$2"
     if test "${string#*$substring}" != "$string"
     then
-        return 0    # $substring is in $string
+        return 0
     else
-        return 1    # $substring is not in $string
+        return 1
     fi
 }
 
-visited="adfadsf"
-v="a"
 
 
+dfs() 
+(
+	# if test "${visited#*$1}" != "$visited"
+	
+	# local v
+	# v=$1
+
+	# echo "DFS $1"
+	
+	visited="$2"
+	if ! contains "$visited" " $1 "; then
+
+		echo "$1 NOT VISITED"
+		visited="$visited $1 "
+		echo "VISITED NODES $visited"
+
+		
+		adj_list=$(echo "$input" | sed -n "/^$1/p")
+		echo "CHILDS $adj_list"
+		# wait
+		# exit
+		for child in $adj_list
+		do
+			# echo $child
+			# wait
+			dfs $child "$visited"
+		done
+	# else 
+	# 	echo "$1 ALREADY VISITED"
+	fi
+)
+
+visited=" "
+dfs from "$visited"
+
+echo "FINAL VISITED $visited"
+
+
+
+
+# visited="adfadsf"
+# v="u"
+
+# dfs $v
 # case $line in (*"$PWD"*)
 # #    whatever your then block had
 # ;;esac
 
-
-if [ "${visited#*$v}" != "$visited" ]
-then 
-	echo YES
-else 
-	visited="$visited $v"
-	echo NO
-	echo "$visited"
-fi
+# !!!!!!!!!!!!!!!!!!!!!!
+# if [ "${visited#*$v}" != "$visited" ]
+# then 
+# 	echo YES
+# else 
+# 	visited="$visited $v"
+# 	echo NO
+# 	echo "$visited"
+# fi
 
 
 # declare -A ADJ_MAP
