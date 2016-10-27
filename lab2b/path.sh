aa7f7f65b7673a1079093ad146f4484a46f62a79
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2016-10-27 16:08:28 +0300

    BASH version without help

--- lab2b/path.sh
+++ lab2b/path.sh
@@ -1,58 +1,36 @@
 #!/bin/bash
-
-GRAPH_from=("1" "2" "3")
-GRAPH_1=("from")
-GRAPH_2=("from" "4")
-GRAPH_3=("from" "4")
-GRAPH_4=("2" "3")
-GRAPH_5=("to")
-GRAPH_to=("5")
-
-# str=a:b:c:d:e
-# arr=(${str//:/ })
-
-
+declare -A ADJ_MAP
+while read line
+do
+	adj_list=${line#* }
+	vertex=${line%% *}
+	ADJ_MAP[$vertex]="$adj_list"
+done < "${1:-/dev/stdin}"
 queue=()
 visited=" "
-
-
 queue+=("from")
-
 while [ ${#queue[@]} -ne 0 ]
 do
 	curr=${queue[0]}
 	unset queue[0]
 	queue=("${queue[@]}")
-
-	echo "CURR ${curr}"
-
-
 	if [[ $visited != *"$curr"* ]]
 	then
   		visited+="$curr "
 	fi
-
-	arr_name="GRAPH_$curr"
-	array="${arr_name}[@]"
-
-	for i in "${!array}"
+	for v in ${ADJ_MAP[$curr]}
 	do
-		echo " ${i} "
-		# read -n1 -r -p "Press space to continue..." key
-		# echo " ${visited} "
-		if [ "$i" == "to" ]
+		if [ "$v" == "to" ]
 		then
 			echo "YES"
 			exit 0
 		fi
-
-		if [[ $visited != *" $i "* ]]
+		if [[ $visited != *" $v "* ]]
 		then
-  			visited+="$i "
-  			queue+=("$i")
+  			visited+="$v "
+  			queue+=("$v")
 		fi
 	done
 done
-
 echo "NO"
 exit 0
