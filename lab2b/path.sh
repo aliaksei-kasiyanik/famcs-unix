3097b8f6f22b6a7d3a163e97a6115a9b20df14d3
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2016-10-28 15:41:24 +0300

    add usage

--- lab2b/path.sh
+++ lab2b/path.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+usage ()
+{
+  echo "Usage: $(basename "$0") [OPTION]"
+  echo "Read from stdin definition of undirected graph like: "
+  echo "----------"
+  echo "from 1 2 3"
+  echo "1 from"
+  echo "2 from 4"
+  echo "3 from 4"
+  echo "4 5 2 3"
+  echo "5 4 to"
+  echo "to 5"
+  echo "----------"
+  echo "and print 'YES' if nodes 'from' and 'to' are connected, otherwise print 'NO'"
+  echo "Options:"
+  echo "    -h, --help  	display this help and exit"
+}
+
+case "$1" in
+	-h | --help )           usage
+                            exit 0
+esac
+
 declare -A ADJ_MAP
 while read line
 do
